f549dc80f18ef6b1588308b3db190be639e906fb
Andrew M Kirfman <andrew.kirfman@login1.(none)>
2017-05-31 00:25:53 -0500

    Initial homework 1 files.  Includes output results from ada

---
+++ HW1/hw1_plotting.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+# --------------------------------------------------------------------------- #
+# Developer: Andrew Kirfman                                                   #
+# Project: CSCE-435 Homework #1                                               #
+#                                                                             #
+# File: ./hw1_plotting.py                                                     # 
+# --------------------------------------------------------------------------- #
+
+import matplotlib.pyplot as plt
+
+if __name__ == "__main__":
+    pass
